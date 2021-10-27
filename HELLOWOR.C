8bb7119bda2e59bae28afd4304b34a1289c5aef1
EstharAlex <74195278+EstharAlex@users.noreply.github.com>
2021-10-27 15:06:51 +0530

    Add files via upload

---
+++ HELLOWOR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// variables here
+int str=0;
+
+//*******************Main*****************
+void main()
+{
+clrscr();
+getch();
+	printf("Hello world");
+	printf("%d",str);
+getch();
+}
+//********************END******************
